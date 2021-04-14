7f1ceed0b0ee31b82c15ca6d763d7103cc3c6e86
aymen2782 <aymen.sellaouti@gmail.com>
2021-04-14 09:32:54 +0100

    innerHTML

--- assets/js/manipDom.js
+++ assets/js/manipDom.js
@@ -13,7 +13,7 @@ function hide(element, timer) {
   }, timer);
 }
 const monDiv = document.querySelector("div");
-hide(monDiv, 1500);
+/* hide(monDiv, 1500); */
 
 const body = document.querySelector("body");
 /* ampoulePlayer(monDiv, ["ampoule", "blueAmpouleLed"], 2000);
@@ -21,7 +21,7 @@ ampoulePlayer(body, ["ampoule", "blueAmpouleLed"], 1000);
  */
 
 const lien = document.querySelector("a");
-updateLinkElement(lien);
+/* updateLinkElement(lien); */
 function updateLinkElement(element) {
   const modify = confirm(
     `Voulez vous modifier le lien suivant : + ${element.href}`
@@ -32,3 +32,29 @@ function updateLinkElement(element) {
     element.href = newLink;
   }
 }
+
+villes = [
+  "Tunis",
+  "Tatouin",
+  "Djerba",
+  "Bizerte",
+  "Mahdia",
+  "Sidi Bouzid",
+  "Kasserine",
+  "Kef",
+];
+
+function showCities(olUlElement, cities, timer) {
+  let i = 0;
+  const intervalId = setInterval(() => {
+    if (i == cities.length) {
+      clearInterval(intervalId);
+    } else {
+      olUlElement.innerHTML += `<li>${cities[i]}</li>`;
+      i++;
+    }
+  }, timer);
+}
+
+const ol = document.querySelector("ol");
+showCities(ol, villes, 2000);
