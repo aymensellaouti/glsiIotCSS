d9142e146c28cbaa5aabd1fbe996c22adc6e262f
aymen2782 <aymen.sellaouti@gmail.com>
2021-04-21 13:01:43 +0100

    manipulate dom

---
+++ assets/js/addImage.js
@@ -0,0 +1,18 @@
+function addImage(src, width, height, classes, parent, isFirst) {
+  //Je crée mon Image
+  const image = document.createElement("img");
+
+  image.src = src;
+  image.width = width;
+  image.height = height;
+
+  for (let i = 0; i < classes.length; i++) {
+    image.classList.add(classes[i]);
+  }
+
+  if (isFirst) {
+    parent.insertBefore(image, parent.firstChild);
+  } else {
+    parent.appendChild(image);
+  }
+}
